91fab33bd1202838c5ec0a1a154275dfe7655730
Dhanmoni Nath <dhanmoninath989@gmail.com>
2020-06-14 22:25:02 +0530

    lesson-5: dynamic(any) types

--- sandbox.js
+++ sandbox.js
@@ -1,37 +1,8 @@
-//explicit types--
-var character;
 var age;
-var isLoggedIn;
-// age = 'luigi' --doesnot work
-age = 30; //fine
-// isLoggedIn = 26 --doesnot work
-isLoggedIn = true; //fine
-//Arrays--
-var ninjas;
-//initialize with something--
-var numbers = [];
-// ninjas = [10, 20] --doesnot work;
-ninjas = ["yoshi", "mario"];
-numbers.push(10); //works
-//union types--
-var mixed = [];
-mixed.push(10);
-mixed.push("hello");
-mixed.push(true);
-console.log(mixed); //[10, "hello", true]
-var uid;
-uid = 1033; //works
-uid = "1099"; //works
-//objects--
-var ninjaOne;
-ninjaOne = {
+//let age: any = 25;
+age = true; //works;
+age = "hello"; //works;
+age = {
     name: "mario",
     age: 20
-};
-// ninjaOne = 'hello' --doesnot work
-// ninjaOne = [] --this.work for type object
-var ninjaTwo;
-ninjaTwo = {
-    name: "mario",
-    age: 20
-};
+}; //works
--- sandbox.ts
+++ sandbox.ts
@@ -1,51 +1,22 @@
-//explicit types--
-let character: string;
-let age: number;
-let isLoggedIn: boolean;
+let age: any;
+//let age: any = 25;
 
-// age = 'luigi' --doesnot work
-age = 30; //fine
+age = true; //works;
+age = "hello"; //works;
+age = {
+  name: "mario",
+  age: 20,
+}; //works
 
-// isLoggedIn = 26 --doesnot work
-isLoggedIn = true; //fine
-
-//Arrays--
-let ninjas: string[];
-//initialize with something--
-let numbers: number[] = [];
-
-// ninjas = [10, 20] --doesnot work;
-ninjas = ["yoshi", "mario"];
-numbers.push(10); //works
-
-//union types--
-let mixed: (string | number | boolean)[] = [];
-
+let mixed: any[] = [];
 mixed.push(10);
-mixed.push("hello");
+mixed.push("mario");
 mixed.push(true);
-console.log(mixed); //[10, "hello", true]
+//[10, mario, true]
 
-let uid: string | number;
-uid = 1033; //works
-uid = "1099"; //works
-
-//objects--
-let ninjaOne: object;
-
-ninjaOne = {
-  name: "mario",
-  age: 20,
+let ninja: {
+  name: any;
+  age: any;
 };
-// ninjaOne = 'hello' --doesnot work
-// ninjaOne = [] --this.work for type object
-
-let ninjaTwo: {
-  name: string;
-  age: number;
-};
-
-ninjaTwo = {
-  name: "mario",
-  age: 20,
-};
+ninja = { name: "yoshi", age: 25 }; //works
+ninja = { name: 25, age: "yoshi" }; //works too
