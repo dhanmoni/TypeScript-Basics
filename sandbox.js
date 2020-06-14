6d3835e306613f3b25e18429c9834b212bae6102
Dhanmoni Nath <dhanmoninath989@gmail.com>
2020-06-14 21:14:06 +0530

    lesson-2: types basics

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,18 @@
 var character = "mario";
-console.log(character);
-var inputs = document.querySelectorAll("input");
-inputs.forEach(function (input) {
-    console.log(input);
-});
+var age = 20;
+var isBlackBelt = true;
+//age = '20' --cannot do this
+age = 30; //--can do this.
+//character = 20 -- cannot do this;
+character = "luigi"; //--can do this;
+//isBlackBelt = 'yes' --cannot do this
+isBlackBelt = false; //-- can do this
+var circ1 = function (diameter) {
+    return diameter * Math.PI; //Math is build into javascript, so TS inherits it as TS is superset of JS
+};
+console.log(circ1("hello")); //doesnot give error directly.
+var circ2 = function (diameter) {
+    return diameter * Math.PI; //Math is build into javascript, so TS inherits it as TS is superset of JS
+};
+//console.log(circ2("hello")); //now gives an error.
+console.log(circ2(7.5)); //now it works as expected!
--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,25 @@
-const character = "mario";
+let character = "mario";
+let age = 20;
+let isBlackBelt = true;
 
-console.log(character);
+//age = '20' --cannot do this
+age = 30; //--can do this.
 
-const inputs = document.querySelectorAll("input");
+//character = 20 -- cannot do this;
+character = "luigi"; //--can do this;
 
-inputs.forEach((input) => {
-  console.log(input);
-});
+//isBlackBelt = 'yes' --cannot do this
+isBlackBelt = false; //-- can do this
+
+const circ1 = (diameter) => {
+  return diameter * Math.PI; //Math is build into javascript, so TS inherits it as TS is superset of JS
+};
+
+console.log(circ1("hello")); //doesnot give error directly.
+
+const circ2 = (diameter: number) => {
+  return diameter * Math.PI; //Math is build into javascript, so TS inherits it as TS is superset of JS
+};
+
+//console.log(circ2("hello")); //now gives an error.
+console.log(circ2(7.5)); //now it works as expected!
