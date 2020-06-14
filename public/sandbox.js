0448c69965f2be4bd0fa2811383a07f26abc3f77
Dhanmoni Nath <dhanmoninath989@gmail.com>
2020-06-14 23:09:58 +0530

    lesson-9: function signature

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,13 +1,30 @@
 "use strict";
-var greet = function (user) {
-    console.log(user.name + " with uid " + user.uid + " says hello ");
+//let greet: Function;
+//Function Signature--
+//example 1
+var greet;
+greet = function (name, greeting) {
+    console.log(name + " says " + greeting);
 };
-var logDetails = function (item, uid) {
-    console.log(item + " has uid of " + uid);
+//example 2
+var calc;
+calc = function (num1, num2, action) {
+    if (action === "add")
+        return num1 + num2;
+    else
+        return num1 - num2;
 };
-var greetAgain = function (user) {
-    console.log(user.name + " with uid " + user.uid + " says hello ");
+//this fn must match the function signature,
+//that is why we need an extra else so that we return a number even if
+//the 'if block' is not executed.
+//example 3
+var logDetails;
+logDetails = function (ninja) {
+    console.log(ninja.name + " is " + ninja.age + " years old");
 };
-var logDetailsAgain = function (item, uid) {
-    console.log(item + " has uid of " + uid);
+var logDetailsWithAlias;
+logDetailsWithAlias = function (ninja) {
+    console.log(ninja.name + " is " + ninja.age + " years old");
 };
+logDetails({ name: "luigi", age: 30 });
+logDetailsWithAlias({ name: "mario", age: 10 });
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,18 +1,40 @@
-type StringOrNum = string | number;
-type objWithName = { name: string; uid: StringOrNum };
+//let greet: Function;
 
-const greet = (user: { name: string; uid: string | number }) => {
-  console.log(`${user.name} with uid ${user.uid} says hello `);
+//Function Signature--
+
+//example 1
+let greet: (a: string, b: string) => void;
+
+greet = (name: string, greeting: string) => {
+  console.log(`${name} says ${greeting}`);
 };
 
-const logDetails = (item: string, uid: string | number) => {
-  console.log(`${item} has uid of ${uid}`);
+//example 2
+
+let calc: (a: number, b: number, c: string) => number;
+
+calc = (num1: number, num2: number, action: string) => {
+  if (action === "add") return num1 + num2;
+  else return num1 - num2;
+};
+//this fn must match the function signature,
+//that is why we need an extra else so that we return a number even if
+//the 'if block' is not executed.
+
+//example 3
+
+let logDetails: (obj: { name: string; age: number }) => void;
+
+logDetails = (ninja: { name: string; age: number }) => {
+  console.log(`${ninja.name} is ${ninja.age} years old`);
+};
+// USING TYPE ALIASES
+type person = { name: string; age: number };
+let logDetailsWithAlias: (obj: person) => void;
+
+logDetailsWithAlias = (ninja: person) => {
+  console.log(`${ninja.name} is ${ninja.age} years old`);
 };
 
-const greetAgain = (user: objWithName) => {
-  console.log(`${user.name} with uid ${user.uid} says hello `);
-};
-
-const logDetailsAgain = (item: string, uid: StringOrNum) => {
-  console.log(`${item} has uid of ${uid}`);
-};
+logDetails({ name: "luigi", age: 30 });
+logDetailsWithAlias({ name: "mario", age: 10 });
