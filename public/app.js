ea2ce9db67dc8c858ed7ac6b4f441c87c450ee05
Dhanmoni Nath <dhanmoninath989@gmail.com>
2020-06-15 13:29:02 +0530

    lesson-13: Modules

--- public/app.js
+++ public/app.js
@@ -1,40 +1,19 @@
-"use strict";
-var Invoice = /** @class */ (function () {
-    // readonly client: string; //readonly allows only to read the value outside or inside the class but not change it.
-    // private details: string; //private only allows to read and write value within the class but not outside the class
-    // public amount: number; //can read and write anywhere.
-    // constructor(c: string, d: string, a: number) {
-    //   this.client = c;
-    //   this.details = d;
-    //   this.amount = a;
-    // }
-    //---SHORTHAND--- WHEN USING ACCESS MODIFIERS
-    function Invoice(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    //---SHORTHAND---
-    Invoice.prototype.format = function () {
-        return this.client + " owes \u20B9" + this.amount + " for " + this.details;
-    };
-    return Invoice;
-}());
-var invOne = new Invoice("mario", "work on mario website", 1000);
-var invTwo = new Invoice("luigi", "work on luigi website", 1500);
-var invoices = [];
+import { Invoice } from "./classes/Invoice.js";
+const invOne = new Invoice("mario", "work on mario website", 1000);
+const invTwo = new Invoice("luigi", "work on luigi website", 1500);
+let invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
-invoices.forEach(function (inv) {
+invoices.forEach((inv) => {
     console.log(inv.client, inv.amount, inv.format());
 });
-var form = document.querySelector(".new-item-form");
+const form = document.querySelector(".new-item-form");
 //console.log(form.children)
-var type = document.querySelector("#type");
-var tofrom = document.querySelector("#tofrom");
-var details = document.querySelector("#details");
-var amount = document.querySelector("#amount");
-form.addEventListener("submit", function (e) {
+const type = document.querySelector("#type");
+const tofrom = document.querySelector("#tofrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+form.addEventListener("submit", (e) => {
     e.preventDefault();
     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,4 @@
-class Invoice {
-  // readonly client: string; //readonly allows only to read the value outside or inside the class but not change it.
-  // private details: string; //private only allows to read and write value within the class but not outside the class
-  // public amount: number; //can read and write anywhere.
-
-  // constructor(c: string, d: string, a: number) {
-  //   this.client = c;
-  //   this.details = d;
-  //   this.amount = a;
-  // }
-
-  //---SHORTHAND--- WHEN USING ACCESS MODIFIERS
-
-  constructor(
-    readonly client: string,
-    private details: string,
-    public amount: number
-  ) {}
-  //---SHORTHAND---
-
-  format() {
-    return `${this.client} owes ₹${this.amount} for ${this.details}`;
-  }
-}
+import { Invoice } from "./classes/Invoice.js";
 
 const invOne = new Invoice("mario", "work on mario website", 1000);
 const invTwo = new Invoice("luigi", "work on luigi website", 1500);
